6860f0fae02192efb4c93fd61633deb476315690
kisumutalent <homiemusa@gmail.com>
2018-12-20 20:04:30 +0300

    Using python data structure to save data in CSV files

---
+++ app.py
@@ -0,0 +1,35 @@
+#import the csv module so we can write data to csv
+import csv
+# imports the random module so we can generate a random number
+import random 
+
+#makes a 2D array called bookings. This is where the collected data.
+# is going to before written to the csv file
+bookings = []
+
+# creates a random number between 0 and 100 for the booking ref
+bookingref = random.randint(0,100)
+
+#conv this number from integer to string as integers con't be written to a csv file
+bookingref = str(bookingref)
+
+# asks then to enter details
+surname = input("please enter your surname")
+forename = input("Please enter your forename")
+film = input("Please enter the film you want see")
+day = input("What day of the week do you want see the film?")
+
+# adds these details to the 2D array called 'bookings'
+#addpend means add
+bookings.append(bookingref)
+bookings.append(surname)
+bookings.append(forename)
+bookings.append(film)
+bookings.append(day)
+
+# takes all the informaion in the 2D array 'bookings' and writes it
+#to the csv file colled 'cinema.csv'
+
+with open("cinama.csv", "a", newline='') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerow(bookings)
