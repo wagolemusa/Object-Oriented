36dd5433b008a64cde247fc3f8176e0df6466528
kisumutalent <homiemusa@gmail.com>
2019-01-18 01:25:18 +0300

    Function takes two parameters and print the square root

---
+++ numbers.py
@@ -0,0 +1,16 @@
+
+    # Create a function called hypotenuse, which takes two numbers as parameters and prints the square root of the sum of their squares.
+    # Call this function with two floats.
+    # Call this function with two integers.
+    # Call this function with one integer and one float.
+import math
+def hypotenuse(a, b):
+	print(math.sqrt(a**2 + b**2))
+
+hypotenuse(12.3, 45.6)
+hypotenuse(12, 8)
+hypotenuse(4, 23)
+
+
+
+
