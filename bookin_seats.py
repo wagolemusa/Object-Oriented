064ba817eb8ed51daa0e4c4c7feb544bd5bacc2b
kisumutalent <homiemusa@gmail.com>
2019-04-05 09:21:53 +0300

    Stadum seats caluclation

---
+++ bookin_seats.py
@@ -0,0 +1,47 @@
+"""
+Threre are seating categories at stadium for a softball game
+class A seats cost 2000ksh, Class B seats Cost 1500 shs
+Class C cost 1000ksh.Write a program that ask how many tickes for class of seats
+were sold, and display the amount of income generated from ticket
+soles
+"""
+def calculateClassTickectSales( classATicketsBought ):
+	classASales = classATicketsBought * 2000
+	return classASales
+
+def calculateClassTickectSales( classBTicketsBought ):
+	classBSales = classBTicketsBought * 1500
+	return classBSales
+
+def calculateClassTickectSales( classCTicketsBought ):
+	classCSales = classCTicketsBought * 1000
+	return classCSales
+
+def calculateTotalSale( SalesA, SalesB, SalesC):
+	totalSales = SalesA + SalesB + SalesC
+	return totalSales
+
+def printSales( SalesA, SalesB, SalesC, totalSales ):
+	print( 	"Class A Seats: KSH" + format(SalesA),\
+					"Class C Seats: KSH" + format(SalesB),\
+					"Class A Seats: KSH" + format(SalesC),\
+					"Total sales:   KSH" + format(totalSales))
+
+
+def main():
+	classATicketsBought = int( input("How many tuckets were bought "+\
+															" for class A ?"))
+	classBTicketsBought = int( input("How many tuckets were bought "+\
+															" for class B ?"))
+	classCTicketsBought = int( input("How many tuckets were bought "+\
+															" for class C ?"))
+
+	SalesA = calculateClassTickectSales (classATicketsBought )
+	SalesB = calculateClassTickectSales (classBTicketsBought )
+	SalesC = calculateClassTickectSales (classCTicketsBought )
+
+	totalSales =  calculateTotalSale(SalesA, SalesB, SalesC)
+
+	printSales(SalesA, SalesB, SalesC, totalSales)
+	
+main()
---
+++ seats.py
@@ -0,0 +1,10 @@
+c = "ABCDE"
+
+r = range(0,3)
+from  pprint import pprint as pp 
+
+x = [{str(y) + ltr: 'Empty' for ltr in c } for y in r]
+
+x[0]['0A']='musa'
+
+pp(x)
