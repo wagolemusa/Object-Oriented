60b086c3f69ce3d7dba3628622dac3cc44e2ea48
kisumutalent <homiemusa@gmail.com>
2019-04-20 09:22:50 +0300

    Add delete to linklist

--- linked_list.py
+++ linked_list.py
@@ -14,6 +14,10 @@ class LinkedList:
 			cur_node = cur_node.next
 
 	def append(self, data):
+		"""
+		Method insert to the node.
+
+		"""
 		new_node = Node(data)
 
 		if self.head is None:
@@ -25,6 +29,9 @@ class LinkedList:
 		last_node.next = new_node
 
 	def prepend(self, data):
+		"""
+		Method Add Data on Top of a Node
+		"""
 		new_node = Node(data)
 
 		new_node.next = self.head
@@ -41,6 +48,49 @@ class LinkedList:
 		new_node.next = prev_node.next
 		prev_node.next = new_node
 
+	def delete_node(self, key):
+		"""
+		Method delete a node
+
+		"""
+		cur_node = self.head
+		if cur_node and cur_node.data == key:
+			self.head = cur_node.next
+			cur_node = None
+			return
+		prev = None
+		while cur_node and cur_node.data != key:
+			prev = cur_node
+			cur_node = cur_node.next
+		if cur_node is None:
+			return
+		prev.next = cur_node.next
+		cur_node = None
+
+	def delete_node_at_pos(self, pos):
+		"""
+		Method Delete a Node in a postion
+
+		"""
+		cur_node = self.head
+		if pos == 0:
+			self.head = cur_node.next
+			cur_node = None
+			return 
+
+		prev = None
+		count = 1
+		while cur_node and count != pos:
+
+			prev = cur_node
+			cur_node = cur_node.next
+			count += 1
+
+		if cur_node is None:
+			return
+
+		prev.next = cur_node.next
+		cur_node = None
 
 
 Llist = LinkedList()
@@ -48,8 +98,9 @@ Llist.append("A")
 Llist.append("B")
 Llist.append("C")
 Llist.append("D")
-# Llist.prepend("E")
+# Llist.prepend("OL")
 # It insert into after B
+Llist.delete_node("B")
 Llist.insert_after_node(Llist.head.next, "E")
 Llist.insert_after_node(Llist.head.next, "P")
 Llist.print_list()
